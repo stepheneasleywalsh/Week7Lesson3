87a5ea9486a8fba1e4fccdfa37a1dc8211a9b46a
stephen.easley-walsh <stephen.easley-walsh@studygroup.com>
2021-11-20 21:56:56 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# Solve Quadratics
+
+## Write a program that solves the quadratic
+
+## An example of what it should do is below
+
+I can solve ax^2 + bx + c = 0 for you!
+
+Please give me the 'a' coefficent: ***0***
+
+Please give me the 'b' coefficent: ***1***
+
+Please give me the 'c' coefficent: ***1***
+
+Sorry, but 'a' cannot be zero.
+
+Please give me the 'a' coefficent: ***1***
+
+Please give me the 'b' coefficent: ***2***
+
+Please give me the 'c' coefficent: ***3***
+
+Sorry, but that quadratic does not have real roots.
+
+Please give me the 'a' coefficent: ***1***
+
+Please give me the 'b' coefficent: ***two 2 bla bla bla***
+
+That is not real number!
+
+Please give me the 'a' coefficent: ***1.5***
+
+Please give me the 'b' coefficent: ***2.6***
+
+Please give me the 'c' coefficent: ***-3.1***
+
+That you, that is a valid input :) OKAY I'LL SOLVE IT NOW.
+
+Root 1: -2.5452904416018893
+
+Root 2: 0.8119571082685563
---
+++ main.py
@@ -0,0 +1,35 @@
+# QUADRATIC SOLVER
+
+#Greet
+print("I can solve ax^2 + bx + c = 0 for you!")
+
+# Input Loop
+validInput = False
+while validInput == False:
+    try:
+        a = float(input("Please give me the 'a' coefficent: "))
+        b = float(input("Please give me the 'b' coefficent: "))
+        c = float(input("Please give me the 'c' coefficent: "))
+        delta = b**2 - 4*a*c
+        if a == 0:
+            print("Sorry, but 'a' cannot be zero.")
+        elif delta < 0:
+            print("Sorry, but that quadratic does not have real roots.")
+        else:
+            print("That you, that is a valid input :) OKAY I'LL SOLVE IT NOW.")
+            validInput = True
+    except:
+        print("That is not real number!")
+
+# Print the Roots or Root
+if delta > 0:
+    root1 = (-b-delta**0.5)/(2*a)
+    root2 = (-b+delta**0.5)/(2*a)
+    print("Root 1:", root1)
+    print("Root 2:", root2)
+else:
+    root = (-b) / (2 * a)
+    print("Root:", root)
+
+# Quit
+quit()
